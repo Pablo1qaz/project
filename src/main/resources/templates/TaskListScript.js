9d8e60d6ec322792797ecede061e6b268f85e8d5
Pablo1qaz <147703596+Pablo1qaz@users.noreply.github.com>
2024-11-15 20:11:27 +0100

    Initial commit

--- src/main/resources/templates/TaskListScript.js
+++
@@ -1,7 +0,0 @@
-const listContainer = document.getElementById('list-container')
-
-listContainer.addEventListener('click',function (e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle('ul li.checked');
-    }
-}, false);
